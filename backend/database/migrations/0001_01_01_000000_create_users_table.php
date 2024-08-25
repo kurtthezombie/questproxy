e83b5114372916cdbc0d6a020a3662e9df14cba3
kurtthezombie <kurt.desmond619@gmail.com>
2024-08-25 22:26:54 +0800

    Added table: gamer, pilot. Added model: gamer, pilot.

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         //still not migrated, wip
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id');
+            $table->id();
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
@@ -25,6 +25,32 @@ return new class extends Migration
             $table->rememberToken();    
             $table->timestamps();
         });
+
+        Schema::create('gamers', function (Blueprint $table) {
+            $table->id();
+            $table->string('gamer_preference')->nullable();
+            //fk user_id
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+
+        Schema::create('ranking', function (Blueprint $table) {
+            $table->id();
+            $table->string('pilot_rank');
+            $table->float('points'); 
+        });
+
+        Schema::create('pilots', function (Blueprint $table) {
+            $table->id();
+            $table->string('skills');
+            $table->string('bio');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('rank_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('rank_id')->references('id')->on('ranking');
+        });
+
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
@@ -47,6 +73,17 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('gamers', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('pilots', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['rank_id']);
+        });
+        
+        Schema::dropIfExists('gamers');
+        Schema::dropIfExists('pilots');
+        Schema::dropIfExists('ranking');
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
