aa8b3c60f89aa533fa9dfe009a21bc4c2e12e22f
kurtthezombie <kurt.desmond619@gmail.com>
2024-09-13 01:50:48 +0800

    Service: create,read,update,delete draft again

--- backend/app/Models/Service.php
+++ backend/app/Models/Service.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'game',
+        'description',
+        'price',
+        'duration',
+        'availability',
+        'service_timestamp',
+        'pilot_id'
+    ];
 }
