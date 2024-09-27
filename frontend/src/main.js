37d67dc9b530f327e48ecda53cbda174e2c2e079
kurtthezombie <kurt.desmond619@gmail.com>
2024-09-27 20:02:50 +0800

    Redo frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,41 +1,11 @@
-import Vue from 'vue'
+import './assets/main.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/tailwind.css'
-import VueRouter from 'vue-router'
-import Routes from './routes'
-import VueResource from 'vue-resource'
-import { store } from "./store/store";
+import router from './router'
 
-Vue.use(VueResource)
-Vue.use(VueRouter)
+const app = createApp(App)
 
-// Intitiate router instance
-export const router = new VueRouter({
-  mode: 'history',
-  routes: Routes
-});
+app.use(router)
 
-router.beforeEach((to, from, next) => {
-  // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/login', '/registration'];
-  const authRequired = !publicPages.includes(to.path);
-  const loggedIn = localStorage.getItem('user');
-
-  if (authRequired && !loggedIn) {
-    return next('/login');
-  }
-
-  if (publicPages.includes(to.path) && loggedIn) {
-    return next('/');
-  }
-
-  next();
-})
-
-Vue.config.productionTip = false
-
-new Vue({
-  store: store,
-  router: router,
-  render: h => h(App),
-}).$mount('#app')
+app.mount('#app')
