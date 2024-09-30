df7a97a1d403a5830d15c102724b105c8583386a
VincentRice <Vincent.villacampo21@gmail.com>
2024-09-30 08:38:02 +0800

    Small changes

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -54,9 +54,7 @@ const submitForm = async () => {
 <template>
   <div class="registration-form">
     <div class="registration-header">
-      <svg class="gaming-icon" viewBox="0 0 24 24" width="40" height="40">
-        <path d="M21 6H3c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-10 7H8v3H6v-3H3v-2h3V8h2v3h3v2zm4.5 2c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4-3c-.83 0-1.5-.67-1.5-1.5S18.67 9 19.5 9s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" fill="#FF69B4"/>
-      </svg>
+      <img class="gaming-icon" src="@/assets/img/qplogo2.png" alt="logo" width="80" height="80" />
       <h1>Registration</h1>
     </div>
     <form @submit.prevent="submitForm">
