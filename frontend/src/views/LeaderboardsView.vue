72677ddd5603a5dea4369097b0c6714ef5eaa870
kurtthezombie <kurt.desmond619@gmail.com>
2024-10-10 16:52:11 +0800

    Leaderboards simple design

--- frontend/src/views/LeaderboardsView.vue
+++ frontend/src/views/LeaderboardsView.vue
@@ -17,14 +17,25 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="h-screen flex flex-col justify-center items-center">
-        <h2 class="text-2xl text-white">Leaderboards</h2>
-        <ol>
-            list apears here dapat:
-            <li v-for="record in records">
-                {{ record.pilot_username }} - {{ record.points }}
-            </li>
-        </ol>
+    <!-- INSERT NAVBAR HERE ->  -->
+    <div class="flex flex-col justify-between">
+        <div class="mt-40 flex justify-center">
+            <h2 class="text-5xl text-white">Leaderboards</h2>
+        </div>
+        <div class="mt-10 flex flex-col justify-center items-center" v-if="records">
+            <table v-if="records"
+                class="w-2/6 shadow-2xl font-[Poppins] border-2 border-red-100 rounded-lg bg-purple-300/5 overflow-hidden">
+                <tbody v-for="(record, index) in records" :key="index">
+                    <tr
+                        class="text-2xl text-white hover:scale-105 hover:bg-purple-200 hover:scale-105 hover:rounded-lg hover:text-black text-center cursor-pointer duration-300">
+                        <td class="py-3 px-10">{{ index + 1 }}</td>
+                        <td class="py-3 px-10">{{ record.pilot_username }}</td>
+                        <td class="py-3 px-10">{{ record.points }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
+
 
 </template>
