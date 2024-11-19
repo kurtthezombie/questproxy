5d77f08766bf491aaf5c8fd2adbddb1c606eb224
kurtthezombie <kurt.desmond619@gmail.com>
2024-11-19 23:11:54 +0800

    Fix observedby in payment model

--- backend/app/Models/Payment.php
+++ backend/app/Models/Payment.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Observers\PaymentObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[ObservedBy([UserObserver::class])]
+#[ObservedBy([PaymentObserver::class])]
 class Payment extends Model
 {
     use HasFactory;
