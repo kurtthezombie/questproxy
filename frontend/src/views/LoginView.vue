8609d0a5704b0c45ecea2f0cf6af6ea4551c86bc
kurtthezombie <kurt.desmond619@gmail.com>
2025-04-14 22:25:07 +0800

    ui tweaks in login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -80,18 +80,27 @@ const handleBack = async () => {
   <div class="min-h-screen flex items-center justify-center bg-gray-900">
     <div class="bg-white backdrop-blur-md p-8 rounded-lg shadow-lg max-w-md w-full">
       <div>
-        <Button class="btn btn-md rounded-md bg-green-200 text-green-800" @click="handleBack">Back</Button>
+        <Button class="btn btn-xs rounded-md text-green-800 bg-transparent border-0 hover:scale-105 hover:bg-green-100 transition " @click="handleBack">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+          </svg>
+        </Button>
       </div>
       <div class="text-center mb-6">
         <router-link to="/" class="block">
           <img src="@/assets/img/qplogo3.png" alt="logo" class="w-20 h-20 mx-auto">
         </router-link>
         <h1 class="text-2xl font-bold text-green-500">QuestProxy</h1>
-        
+
         <!-- Success message -->
-        <div v-if="message" class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-300 dark:border-green-800" role="alert">
-          <svg class="flex-shrink-0 inline w-4 h-4 me-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        <div v-if="message"
+          class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-300 dark:border-green-800"
+          role="alert">
+          <svg class="flex-shrink-0 inline w-4 h-4 me-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 20 20">
+            <path
+              d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
           </svg>
           <span class="sr-only">Info</span>
           <div>
@@ -100,9 +109,13 @@ const handleBack = async () => {
         </div>
 
         <!-- Error message -->
-        <div v-if="errorMessage" class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-          <svg class="flex-shrink-0 inline w-4 h-4 me-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        <div v-if="errorMessage"
+          class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+          role="alert">
+          <svg class="flex-shrink-0 inline w-4 h-4 me-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 20 20">
+            <path
+              d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
           </svg>
           <span class="sr-only">Error</span>
           <div>
@@ -120,13 +133,14 @@ const handleBack = async () => {
           <input type="password" v-model="password" placeholder="Password" required
             class="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500">
         </div>
-        <button type="submit" class="w-full p-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300">
+        <button type="submit"
+          class="w-full p-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300">
           Login
         </button>
       </form>
 
-        <!-- Divider -->
-        <div class="border-t border-gray-200 dark:border-gray-300 my-5"></div>
+      <!-- Divider -->
+      <div class="border-t border-gray-200 dark:border-gray-300 my-5"></div>
 
       <router-link class="block text-center text-sm mt-4 hover:underline text-gray-600" to="/signup">
         Don't have an account? <span class="text-green-500">Sign Up</span>
