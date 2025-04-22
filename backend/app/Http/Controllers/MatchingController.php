69eb665acf46f2221d23c980c96ce42eec276fd7
kurtthezombie <kurt.desmond619@gmail.com>
2025-04-23 00:40:35 +0800

    add find a pilot match and return pilot details function

--- backend/app/Http/Controllers/MatchingController.php
+++ backend/app/Http/Controllers/MatchingController.php
@@ -12,32 +12,45 @@ use Str;
 class MatchingController extends Controller
 {
     use ApiResponseTrait;
+
     public function matchPilot(Request $request){
-        //game and task details from the form input
-        //validate
         $request->validate([
-            'game' => 'required|string',
-            'task' => 'required|text',
+            'game' => 'required|int',
+            'service' => 'required|string',
             'points' => 'required|int' 
         ]);
-        $game = Str::snake($request->game);
+
         //query matching pilots
-        $matchingPilot = Pilot::whereHas('services', function($query) use ($request, $game) {
-            $query->where('game',$game)
-                ->where('description','like', '%' . $request->task . '%')
-                ->where('points','>=',$request->points);
+        $matchingPilot = Pilot::whereHas('services', function($query) use ($request) {
+            $query->where('category_id', $request->game)
+                ->where('description', 'like', '%' . $request->service . '%')
+                ->where('availability', true);
+        })->whereHas('rank', function($query) use ($request) {
+            $query->where('points', '>=', $request->points);
         })->get();
 
-        //check results
+        //return if no pilots are found
         if ($matchingPilot->isEmpty()){
-            return $this->successResponse('No matching pilots found.', 404);
+            return $this->successResponse('No matching pilots found.', 200);
         }
-        //trigger notifications
-        $pilot_user = $matchingPilot->user;
+
+        //select random pilot from the collection
+        $randomPilot = $matchingPilot->random();
+
+        //get pilot and your user records
+        $pilotUser = $randomPilot->user;
         $user = $request->user();
-        $pilot_user->notify(new PilotMatchedNotification($user));
-        event(new NotificationBroadcastEvent($user));
+
+        //$this->sendNotifToPilot($pilotUser, $user);
+        
         //return response
-        return $this->successResponse('Matching pilots found.', 200, ['pilot' => $pilot_user]);
+        return $this->successResponse('Matching pilots found.', 200, ['pilot' => $pilotUser, 'pilot_details' => $randomPilot]);
+    }
+
+    private function sendNotifToPilot($pilot, $gamer)
+    {
+        //send notification
+        $pilot->notify(new PilotMatchedNotification($gamer));
+        event(new NotificationBroadcastEvent($gamer));
     }
 }
