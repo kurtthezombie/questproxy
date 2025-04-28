b19c681a2cf3d78095ef3ef644f8be8fd39ae5a6
aresspeed <nairyonac@gmail.com>
2025-04-28 14:45:41 +0800

    improved ui: mybookingview

--- frontend/src/views/MyBookingsView.vue
+++ frontend/src/views/MyBookingsView.vue
@@ -36,28 +36,35 @@ onMounted(() => {
 </script>
 
 <template>
-  <NavBar />
-  <div class="min-h-screen bg-gray-900 flex items-center justify-start text-white flex-col">
-    <div class="fixed top-0 left-0 w-full bg-gray-900 z-10 py-4 flex flex-col items-center" style="margin-top: 80px;">
-      <h2 class="mb-4 text-3xl font-bold text-white">My Bookings</h2>
-      <div class="flex gap-4">
-        <button @click="filter = 'all'" :class="{ 'font-bold': filter === 'all', 'btn': true, 'btn-primary': filter !== 'all' }">All</button> <button @click="filter = 'in_progress'" :class="{ 'font-bold': filter === 'in_progress', 'btn btn-warning': true }">In Progress</button>
-        <button @click="filter = 'completed'" :class="{ 'font-bold': filter === 'completed', 'btn btn-success': true }">Completed</button> </div>
+  <div class="min-h-screen bg-gray-900 text-white">
+    <NavBar />
+    <div class="container mx-auto py-5 max-w-7xl">
+      <div class="flex justify-between items-center px-4 mt-10 mb-4">
+        <div>
+          <h1 class="text-5xl font-bold text-white">My Bookings</h1>
+          <p class="text-gray-300 text-lg mt-3">Manage your gaming bookings</p>
+        </div>
+      </div>
+        <div class="flex gap-4 mt-4 ml-4">
+          <button @click="filter = 'all'" :class="{ 'font-bold shadow-none': filter === 'all', 'btn': true, 'btn-primary': filter !== 'all' }">All</button> 
+          <button @click="filter = 'in_progress'" :class="{ 'font-bold': filter === 'in_progress', 'btn bg-yellow-500 bg-opacity-30 border border-none shadow-none': true, 'text-yellow-300': true }">In Progress</button>
+          <button @click="filter = 'completed'" :class="{ 'font-bold shadow-none': filter === 'completed', 'btn bg-green-500 bg-opacity-30 border border-none shadow-none text-green-300': true }">Completed</button> 
+        </div>
     </div>
 
-    <div class="pt-40 w-full flex flex-col items-center">
-      <p v-if="filteredBookings.length === 0" class="text-center text-xl cursor-pointer hover:scale-105 duration-300" @click="redirectToHome">
-        <span v-if="filter === 'all'">You have not booked a service yet. Maybe try booking <span class="text-green-400">here</span>.</span>
-        <span v-else>No {{ filter.replace('_', ' ') }} bookings found.</span> </p>
-      <div v-else class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 overflow-y-auto max-h-[calc(100vh-200px)] px-4">
-        <MyBookingCard v-for="booking in filteredBookings" :key="booking.booking.id" :serviceTitle="booking.serviceTitle"
-          :gameName="booking.gameTitle" :pilotName="booking.pilotName" :status="booking.booking.status"
-          :bookedOn="dayjs(booking.created_at).format('MMMM D,YYYY')"
-          :progress="booking.booking.progress"
-          :serviceId="booking.booking.service_id"
-          :bookingId="booking.booking.id"
-          />
+      <div class="pt-40 w-full flex flex-col items-center">
+        <p v-if="filteredBookings.length === 0" class="text-center text-xl cursor-pointer hover:scale-105 duration-300" @click="redirectToHome">
+          <span v-if="filter === 'all'">You have not booked a service yet. Maybe try booking <span class="text-green-400">here</span>.</span>
+          <span v-else>No {{ filter.replace('_', ' ') }} bookings found.</span> </p>
+        <div v-else class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 overflow-y-auto max-h-[calc(100vh-200px)] px-4">
+          <MyBookingCard v-for="booking in filteredBookings" :key="booking.booking.id" :serviceTitle="booking.serviceTitle"
+            :gameName="booking.gameTitle" :pilotName="booking.pilotName" :status="booking.booking.status"
+            :bookedOn="dayjs(booking.created_at).format('MMMM D,YYYY')"
+            :progress="booking.booking.progress"
+            :serviceId="booking.booking.service_id"
+            :bookingId="booking.booking.id"
+            />
+        </div>
       </div>
     </div>
-     </div>
 </template>
